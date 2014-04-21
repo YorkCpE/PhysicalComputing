e9cb85e0cb56f6a42e328d4f61aabaf56eb22dcc
Jason Forsyth <Jason Forsyth@netlenovo>
2014-04-21 19:41:33 -0400

    Initial Commit

---
+++ src/physicalcomputing/HelloWorld.java
@@ -0,0 +1,65 @@
+package physicalcomputing;
+
+import netP5.NetAddress;
+import oscP5.OscEventListener;
+import oscP5.OscMessage;
+import oscP5.OscP5;
+import oscP5.OscStatus;
+import processing.core.PApplet;
+
+
+public class HelloWorld extends PApplet implements OscEventListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7274157594990025653L;
+
+	/*
+	 * BEGIN PROCESSING SOURCE
+	 */
+	OscP5 server;
+	int listeningPort=9000;
+
+	public void setup() 
+	{
+		size(800,600);
+		background(0);
+		textSize(10);
+		server=new OscP5(this, listeningPort);
+	}
+
+	public void draw() 
+	{
+		if(mousePressed)
+		{
+				NetAddress broadcast = new NetAddress("255.255.255.255", listeningPort);
+				OscMessage message = new OscMessage("/helloWorld");
+			
+				OscP5.flush(message, broadcast);
+		}
+	}
+
+	@Override
+	public void oscEvent(OscMessage message) 
+	{
+		if(message.checkAddrPattern("/helloWorld")==true)
+		{
+			text("Hello World!",width/2,height/2);
+		}
+	}
+
+	@Override
+	public void oscStatus(OscStatus status) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/*
+	 * END PROCESSING SOURCE
+	 */
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { physicalcomputing.HelloWorld.class.getName() });
+	}
+}
